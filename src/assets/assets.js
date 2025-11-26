a98f517d2aa5b7390620a3f9c2755eebfa5b933a
snYatharth2005 <139547990+snYatharth2005@users.noreply.github.com>
2025-11-26 15:56:26 +0530

    Fix case of leetcode image import

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -3,7 +3,7 @@ import github from './github-logo.png';
 import linkedin from "./linkedin.png";
 import twitter from "./twitter.png";
 import logo from "./logo.png";
-import leetcode from "./LeetCode.png"
+import leetcode from "./leetcode.png"
 import user from "./userIcon.png"
 
 export default contact;
